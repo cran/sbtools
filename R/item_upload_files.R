21bbe901a30bc1984d6bb6796549fec95bf57e45
David Blodgett <dblodgett@usgs.gov>
2022-05-26 17:40:02 +0000

    version 1.1.18

--- R/item_upload_files.R
+++ R/item_upload_files.R
@@ -7,6 +7,11 @@
 #' @param files A string vector of paths to files to be uploaded
 #' @param scrape_files logical should the files be scraped for metadata? 
 #' If TRUE, sciencebase will attempt to create extensions based on the files.
+#' 
+#' For example, for shapefiles, this will result in a shapefile extension 
+#' to be returned as a facet of the sciencebase item. 
+#' See item: "58069258e4b0824b2d1d422e" for an example.
+#' 
 #' @export
 #' @return An object of class \code{sbitem}
 #' @examples \dontrun{
@@ -82,8 +87,11 @@ item_append_files = function(sb_id, files, ..., scrape_files = TRUE, session=cur
 									 body = multi_file_body(files), 
 									 session = session)
   
-	item <- as.sbitem(content(r))
-	
+	if(!is.null(r)) {
+		item <- as.sbitem(content(r))
+	} else {
+		return(NULL)
+	}
 	return(check_upload(item, files))
 	
 }
@@ -111,4 +119,96 @@ multi_file_body <- function(files){
 	}
 	names(body) = rep('file', length(body))
 	return(body)
+}
+
+#' @title Upload File to Item Cloud Storage
+#' @description Adds a file to an item in cloud storage
+#'
+#' @template manipulate_item
+#' @inheritParams item_upload_create
+#' @param status logical display upload status?
+#'
+#' @return Success message invisibly. NOTE: cloud processing
+#' can take some time so the added file may not appear immediately. 
+#' For this reason, a sciencebase item json is NOT returned as is
+#' done with other similar functions.
+#'
+#' @import httr
+#' @importFrom mime guess_type
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#'
+#' @examples \dontrun{
+#' res <- item_create(user_id(), "testing 123")
+#' cat("foo bar", file = "foobar.txt")
+#' item_append_files(res$id, "foobar.txt")
+#' }
+#' @export
+item_upload_cloud <- function(sb_id, files, ..., status = TRUE, session=current_session()) {
+
+	try(sb_id <- sb_id$id, silent = TRUE)
+	
+	for(file in files) {
+		if(status)
+			message(paste("Uploading:", file))
+		
+		mimetype <- guess_type(file, unknown = 'application/octet-stream')
+		
+		cloud_upload(file, mimetype, sb_id)
+	}
+	
+	return(invisible("Success"))
+	
+}
+
+cloud_upload <- function(file, mimetype, itemid, chunk_size_bytes = pkg.env$chunk_size_bytes,
+												 session = current_session(), status = TRUE) {
+	
+	f_size_bytes <- file.size(file)
+	f_chunks <- as.integer(f_size_bytes / chunk_size_bytes) + 1
+	
+	f_path <- paste(itemid, basename(file), sep = "/")
+	
+	gql <- httr::handle(url = pkg.env$graphql_url)
+	
+	session_id = create_multipart_upload_session(
+		f_path, mimetype, 
+		session_details(session = session)$username, gql)
+		
+	refresh_token_before_expired()
+	
+	f <- file(description = file, open = "rb")
+	
+	on.exit(close(f), add = TRUE)
+	
+	part_number <- 0
+	parts_header <- list()
+	
+	if(status)
+		pb <- txtProgressBar(0, f_chunks, 1, "-", width = 50, style = 3)
+	
+	while ( length(chunk <- readBin(con = f, what = "raw", 
+																	n = chunk_size_bytes)) > 0 ) {
+		
+		part_number <- part_number + 1
+		
+		refresh_token_before_expired()
+		
+		presignedUrl <- get_presigned_url_for_chunk(f_path, session_id, part_number, gql)
+		
+		put_chunk <- httr::PUT(presignedUrl, body = chunk)
+		
+		if(!put_chunk$status_code == 200) stop(paste("Error uploading file. \n",
+																					 "status:", put_chunk$status_code,
+																					 "content:", rawToChar(put_chunk$content)))
+		parts_header[[part_number]] <- list(ETag = put_chunk$headers$ETag,
+																				PartNumber = part_number)
+		if(status)
+			setTxtProgressBar(pb, part_number)
+	}
+	
+	if(status)
+		close(pb)
+	
+	return(invisible(complete_multipart_upload(f_path, session_id, parts_header, gql)))
+	
 }
